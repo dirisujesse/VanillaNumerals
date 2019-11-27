25aee671fa15ba4508961d271a84c0a916a38c52
dirisujesse <dirisujesse@gmail.com>
2019-11-27 20:33:47 +0100

    Self Implemented Conversion Logic Update

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
-/* 
-    I must state that this solution is inspired by https://ourcodeworld.com/articles/read/353/how-to-convert-numbers-to-words-number-spelling-in-javascript
-    I changed the parseInt call to the + literal to catch alphanumeric strings
-    I implemented logic for handling numbers less than zero
-    I implemented logic to handle decimals / floats
-    I also parse the return string to be in title case
-    I also changed var declarations to let declarations
+/*
+*    Self Implemented solution to Number Conversion Problem
+*    Solution leverages recursion
+*    Author: Dirisu Jesse Bayodele
 */
 
-function convertNum(n, custom_join_character) {
+let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+let numDict = {
+    "0": "Zero",
+    "1": "One",
+    "2": "Two",
+    "3": "Three",
+    "4": "Four",
+    "5": "Five",
+    "6": "Six",
+    "7": "Seven",
+    "8": "Eight",
+    "9": "Nine",
+}
+
+function convertNum(n) {
     n = n.toString()
     let englishPhrase, preDecimal, postDecimal;
     if (n && +n === 0) {
@@ -16,99 +28,102 @@ function convertNum(n, custom_join_character) {
     if (!n || isNaN(+n)) {
         return "The provided value appears invalid, provide a valid number to convert";
     }
-    let isSubZero = +n < 0;
+    let isSubZero = n.startsWith('-');
+    console.log({
+        isSubZero,
+        n
+    });
+    n = isSubZero ? n.slice(1) : n;
+    console.log(n)
     if (n.includes('.')) {
         let words = n.split('.');
         if (+words[1]) {
-            preDecimal = handlePreDecimalNumber(words[0], custom_join_character);
+            preDecimal = convertPreDecimalNumber(words[0]);
             postDecimal = handleDecimal(words[1]);
             englishPhrase = `${isSubZero ? 'Minus ' : ''}${preDecimal} point ${postDecimal}`;
         } else {
-            preDecimal = handlePreDecimalNumber(words[0], custom_join_character);
+            preDecimal = convertPreDecimalNumber(words[0]);
             englishPhrase = `${isSubZero ? 'Minus ' : ''}${preDecimal}`;
         }
     } else {
-        preDecimal = handlePreDecimalNumber(n, custom_join_character);
+        preDecimal = convertPreDecimalNumber(n);
         englishPhrase = `${isSubZero ? 'Minus ' : ''}${preDecimal}`;
     }
     return englishPhrase;
 }
 
 function handleDecimal(string) {
-    let numDict = {
-        "0": "Zero",
-        "1": "One",
-        "2": "Two",
-        "3": "Three",
-        "4": "Four",
-        "5": "Five",
-        "6": "Six",
-        "7": "Seven",
-        "8": "Eight",
-        "9": "Nine",
-    }
-
     return string.split('').map(wrd => numDict[wrd]).join(' ');
 }
 
-function handlePreDecimalNumber(string, custom_join_character) {
-    let units, tens, scales, start, end, chunks, chunksLen, chunk, ints, i, word, words;
-    let and = custom_join_character || 'and';
-    units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    scales = ['', 'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion', 'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion', 'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion', 'octodecillion', 'novemdecillion', 'vigintillion', 'centillion'];
-
-    start = string.length;
-    chunks = [];
-    while (start > 0) {
-        end = start;
-        chunks.push(string.slice((start = Math.max(0, start - 3)), end));
-    }
-
-    chunksLen = chunks.length;
-    if (chunksLen > scales.length) {
+function convertPreDecimalNumber(numString) {
+    if (!+numString) {
         return '';
     }
-
-    words = [];
-    for (i = 0; i < chunksLen; i++) {
-        chunk = +chunks[i];
-        if (chunk) {
-            ints = chunks[i].split('').reverse().map(parseFloat);
-            if (ints[1] === 1) {
-                ints[0] += 10;
-            }
-            if ((word = scales[i])) {
-                words.push(word);
-            }
-            if ((word = units[ints[0]])) {
-                words.push(word);
-            }
-            if ((word = tens[ints[1]])) {
-                words.push(word);
-            }
-            if (ints[0] || ints[1]) {
-                if (ints[2] || !i && chunksLen) {
-                    words.push(and);
-                }
-
-            }
-            if ((word = units[ints[2]])) {
-                words.push(word + ' hundred');
-            }
-
-        }
-
+    const lenNum = numString.length;
+    if (lenNum === 1 || +numString <= 19) {
+        return units[+numString];
+    } else if (lenNum === 2) {
+        return `${tens[+numString[0]]} ${units[+numString[1]]}`.trim()
+    } else if (lenNum === 3) {
+        let [H, T] = [numString[0], +numString.slice(1)];
+        let tailString = convertPreDecimalNumber(T.toString());
+        tailString = tailString ? `and ${tailString}` : '';
+        return `${units[H]} Hundred ${tailString}`.trim()
+    } else if ([4, 5, 6].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Thousand", [4, 5, 6]);
+    } else if ([7, 8, 9].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Million", [7, 8, 9]);
+    } else if ([10, 11, 12].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Billion", [10, 11, 12]);
+    } else if ([13, 14, 15].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Trillion", [13, 14, 15]);
+    } else if ([16, 17, 18].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Quadrillion", [16, 17, 18]);
+    } else if ([19, 20, 21].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Quintillion", [19, 20, 21]);
+    } else if ([22, 23, 24].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Sextillion", [22, 23, 24]);
+    } else if ([25, 26, 27].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Septillion", [25, 26, 27]);
+    } else if ([28, 29, 30].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Octillion", [28, 29, 30]);
+    } else if ([31, 32, 33].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Nonillion", [31, 32, 33]);
+    } else if ([34, 35, 36].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Decillion", [34, 35, 36]);
+    } else if ([37, 38, 39].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Undecillion", [37, 38, 39]);
+    } else if ([40, 41, 42].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Duodecillion", [40, 41, 42]);
+    } else if ([43, 44, 45].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Tredecillion", [43, 44, 45]);
+    } else if ([46, 47, 48].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Quattuordecillion", [46, 47, 48]);
+    } else if ([49, 50, 51].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Quindecillion", [49, 50, 51]);
+    } else if ([52, 53, 54].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Sexdecillion", [52, 53, 54]);
+    } else if ([55, 56, 57].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Septendecillion", [55, 56, 57]);
+    } else if ([58, 59, 60].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Octodecillion", [58, 59, 60]);
+    } else if ([61, 62, 63].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Novemdecillion", [61, 62, 63]);
+    } else if ([64, 65, 66].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Vigintillion", [64, 65, 66]);
+    } else if ([67, 68, 69].includes(lenNum)) {
+        return handleThanHundredNums(numString, lenNum, "Centillion", [67, 68, 69]);
+    } else {
+        return "Oops that's a large one, our beta app can't handle such massive numbers";
     }
+}
 
-    words = words.map(wrd => {
-        if (wrd !== and) {
-            return wrd.length > 1 ? `${wrd[0].toUpperCase()}${wrd.slice(1)}` : wrd.toString()
-        } else {
-            return wrd;
-        }
-    }).reverse().join(' ');
-    return +string < 100 ? words.replace(custom_join_character, '') : words;
+function handleThanHundredNums(numString, lenNum, scale, numRange) {
+    const [minuend, startrange, midrange, _] = [numRange[0] - 1, ...numRange]
+    const [H, T] = [lenNum === startrange ? units[numString[0]] : lenNum === midrange ? convertPreDecimalNumber(numString.slice(0, 2)) : convertPreDecimalNumber(numString.slice(0, 3)), +numString.slice(lenNum - minuend)];
+    const tailString = `${T === 0 || T >= 100 ? '' : 'and'} ${convertPreDecimalNumber(T.toString())}`.trim();
+    return `${H} ${scale} ${tailString}`.trim()
 }
 
 (() => {
--- test/spec.js
+++ test/spec.js
@@ -3,50 +3,50 @@ let converter = require("../index");
 describe("Number Conversion Test", () => {
     it("returns failure message if provided input is non numeric or empty string", () => {
         let results = [
-            converter.convertNum("", "and"),
-            converter.convertNum("--1000", "and"),
-            converter.convertNum("++1000", "and"),
-            converter.convertNum("1000++", "and"),
-            converter.convertNum("1000--", "and"),
-            converter.convertNum("11abc", "and"),
-            converter.convertNum("abc11", "and"),
+            converter.convertNum(""),
+            converter.convertNum("--1000"),
+            converter.convertNum("++1000"),
+            converter.convertNum("1000++"),
+            converter.convertNum("1000--"),
+            converter.convertNum("11abc"),
+            converter.convertNum("abc11"),
         ];
         let isAllErrorMessage = results.every(it => it === "The provided value appears invalid, provide a valid number to convert");
         expect(isAllErrorMessage).toBe(true)
     });
     it("returns Zero if provided input is 0 or -0", () => {
         let results = [
-            converter.convertNum("0", "and"),
-            converter.convertNum("-0", "and"),
-            converter.convertNum(0, "and"),
+            converter.convertNum("0"),
+            converter.convertNum("-0"),
+            converter.convertNum(0),
         ];
         let isAllErrorMessage = results.every(it => it.toUpperCase() === "ZERO");
         expect(isAllErrorMessage).toBe(true)
     });
     it("starts with Minus if input is less than 0", () => {
         let results = [
-            converter.convertNum("-20", "and"),
-            converter.convertNum("-1", "and"),
-            converter.convertNum(-120000000, "and"),
+            converter.convertNum("-20"),
+            converter.convertNum("-1"),
+            converter.convertNum(-120000000),
         ];
         let isAllErrorMessage = results.every(it => it.toUpperCase().startsWith("MINUS"));
         expect(isAllErrorMessage).toBe(true)
     });
     it("handle decimals and returns post decimal numbers literally if they are not all zeros", () => {
         let results = [
-            converter.convertNum("-20.001", "and"),
-            converter.convertNum("100.222", "and"),
-            converter.convertNum(200.102, "and"),
-            converter.convertNum(200.002, "and"),
+            converter.convertNum("-20.001"),
+            converter.convertNum("100.222"),
+            converter.convertNum(200.102),
+            converter.convertNum(200.002),
         ];
         let isAllErrorMessage = results.every(it => it.toUpperCase().includes("POINT"));
         expect(isAllErrorMessage).toBe(true)
     });
     it("handle decimals and does not return post decimal numbers literally if they are all zeros", () => {
         let results = [
-            converter.convertNum("-20.000", "and"),
-            converter.convertNum("100.0", "and"),
-            converter.convertNum(200.00000000000, "and"),
+            converter.convertNum("-20.000"),
+            converter.convertNum("100.0"),
+            converter.convertNum(200.00000000000),
         ];
         let isAllErrorMessage = results.every(it => !it.toUpperCase().includes("POINT"));
         expect(isAllErrorMessage).toBe(true)
