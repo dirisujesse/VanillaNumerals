3994b3cad5e9f08d07cf0ff9cf69070b4f8db3ab
dirisujesse <dirisujesse@gmail.com>
2019-11-29 12:21:38 +0100

    Code refactor and some comments added

--- index.js
+++ index.js
@@ -4,53 +4,76 @@
 *    Author: Dirisu Jesse Bayodele
 */
 
-let units = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-let numDict = {
-    "0": "Zero",
-    "1": "One",
-    "2": "Two",
-    "3": "Three",
-    "4": "Four",
-    "5": "Five",
-    "6": "Six",
-    "7": "Seven",
-    "8": "Eight",
-    "9": "Nine",
+const units = [
+    '', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+    'nine', 'ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
+    'sixteen', 'seventeen', 'eighteen', 'nineteen'
+];
+const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+const numDict = {
+    "0": "Zero", "1": "One",
+    "2": "Two", "3": "Three",
+    "4": "Four", "5": "Five",
+    "6": "Six", "7": "Seven",
+    "8": "Eight", "9": "Nine",
 }
 
+const errors = {
+    unsafeNum: "This is a pretty large number, don't expect much accuracy as our engine is not used to handling such massive figures",
+    isNanOrInvalid: "The provided value appears invalid, provide a valid number to convert",
+    unhandled: "Oops can't handle this, is this a valid number?, number may be too large",
+};
+
+// Scales correspond to lengths of numbers from thousands to centillions
+// With sub scales corresponding to units tens and hundreds subscales within the number scales
+const scales = [
+    [ 4,  5,  6], [ 7,  8,  9], [10, 11, 12],
+    [13, 14, 15], [16, 17, 18], [19, 20, 21],
+    [22, 23, 24], [25, 26, 27], [28, 29, 30],
+    [31, 32, 33], [34, 35, 36], [37, 38, 39], [40, 41, 42],
+    [43, 44, 45], [46, 47, 48], [49, 50, 51],
+    [52, 53, 54], [55, 56, 57], [58, 59, 60],
+    [61, 62, 63], [64, 65, 66], [67, 68, 69]
+];
+const scaleNames = [
+    'thousand', 'million', 'billion', 'trillion', 'quadrillion', 'quintillion',
+    'sextillion', 'septillion', 'octillion', 'nonillion', 'decillion', 'undecillion', 'duodecillion',
+    'tredecillion', 'quatttuor-decillion', 'quindecillion', 'sexdecillion', 'septen-decillion',
+    'octodecillion', 'novemdecillion', 'vigintillion', 'centillion'
+];
+
 function convertNum(n) {
-    var upperLimit = 2 ** 53 - 1;
-    var isUnsafe = false;
-    if (n > upperLimit) {
-        isUnsafe = true
-        alert("This is a pretty large number, don't expect much accuracy as our engine is not used to handling such massive figures")
+    n = typeof n !== 'string' ? n.toString() : n;
+    // We declare upperlimit variable to check for numbers greater han native JS number bound
+    var upperLimit = (2 ** 53) - 1;
+    if (Math.abs(+n) > upperLimit) {
+        alert(errors.unsafeNum)
     }
-    n = !isUnsafe ? n.toString() : BigInt(n).toString();
-    let englishPhrase, preDecimal, postDecimal;
     if (n && +n === 0) {
         return "Zero";
     }
     if (!n || isNaN(+n)) {
-        return "The provided value appears invalid, provide a valid number to convert";
+        return errors.isNanOrInvalid;
     }
     let isSubZero = n.startsWith('-');
+    // pruning the ninus sign when the number is less than zero
     n = isSubZero ? n.slice(1) : n;
     if (n.includes('.')) {
+        let preDecimal, postDecimal;
         let words = n.split('.');
         if (+words[1]) {
             preDecimal = convertPreDecimalNumber(words[0]);
             postDecimal = handleDecimal(words[1]);
-            englishPhrase = `${isSubZero ? 'Minus ' : ''}${preDecimal || 'Zero'} point ${postDecimal}`;
+            return `${isSubZero ? 'Minus ' : ''}${preDecimal || 'Zero'} point ${postDecimal}`;
         } else {
             preDecimal = convertPreDecimalNumber(words[0]);
-            englishPhrase = `${isSubZero ? 'Minus ' : ''}${preDecimal || 'Zero'}`;
+            return `${isSubZero ? 'Minus ' : ''}${preDecimal || 'Zero'}`;
         }
     } else {
-        preDecimal = convertPreDecimalNumber(n);
-        englishPhrase = `${isSubZero ? 'Minus ' : ''}${preDecimal || 'Zero'}`;
+        let englishPhrase = convertPreDecimalNumber(n);
+        return `${isSubZero ? 'Minus ' : ''}${englishPhrase || 'Zero'}`;
     }
-    return englishPhrase;
 }
 
 function handleDecimal(string) {
@@ -62,9 +85,8 @@ function convertPreDecimalNumber(strng) {
         return '';
     }
     // I parse strng as BigInt to handle numbers beyond native JS Number range
-    // Accuracy is lost as JS cannot handle very large numbers
-    strng = BigInt(+strng).toString()
-    const numString = strng;
+    // Accuracy may be lost over (2**53) - 1 as JS cannot handle very large numbers
+    const numString = BigInt(strng).toString();
     const lenNum = numString.length;
     if (lenNum === 1 || +numString <= 19) {
         return units[+numString];
@@ -75,59 +97,29 @@ function convertPreDecimalNumber(strng) {
         let tailString = convertPreDecimalNumber(T.toString());
         tailString = tailString ? `and ${tailString}` : '';
         return `${units[H]} Hundred ${tailString}`.trim()
-    } else if ([4, 5, 6].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Thousand", [4, 5, 6]);
-    } else if ([7, 8, 9].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Million", [7, 8, 9]);
-    } else if ([10, 11, 12].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Billion", [10, 11, 12]);
-    } else if ([13, 14, 15].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Trillion", [13, 14, 15]);
-    } else if ([16, 17, 18].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Quadrillion", [16, 17, 18]);
-    } else if ([19, 20, 21].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Quintillion", [19, 20, 21]);
-    } else if ([22, 23, 24].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Sextillion", [22, 23, 24]);
-    } else if ([25, 26, 27].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Septillion", [25, 26, 27]);
-    } else if ([28, 29, 30].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Octillion", [28, 29, 30]);
-    } else if ([31, 32, 33].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Nonillion", [31, 32, 33]);
-    } else if ([34, 35, 36].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Decillion", [34, 35, 36]);
-    } else if ([37, 38, 39].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Undecillion", [37, 38, 39]);
-    } else if ([40, 41, 42].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Duodecillion", [40, 41, 42]);
-    } else if ([43, 44, 45].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Tredecillion", [43, 44, 45]);
-    } else if ([46, 47, 48].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Quattuordecillion", [46, 47, 48]);
-    } else if ([49, 50, 51].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Quindecillion", [49, 50, 51]);
-    } else if ([52, 53, 54].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Sexdecillion", [52, 53, 54]);
-    } else if ([55, 56, 57].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Septendecillion", [55, 56, 57]);
-    } else if ([58, 59, 60].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Octodecillion", [58, 59, 60]);
-    } else if ([61, 62, 63].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Novemdecillion", [61, 62, 63]);
-    } else if ([64, 65, 66].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Vigintillion", [64, 65, 66]);
-    } else if ([67, 68, 69].includes(lenNum)) {
-        return handleThanHundredNums(numString, lenNum, "Centillion", [67, 68, 69]);
-    } else {
-        return "Oops that's a large one, our beta app can't handle such massive numbers";
+    }  else {
+        const numScale = scales.find(it => it.includes(lenNum));
+        if (numScale && numScale !== undefined) {
+            const scaleName = scaleNames[scales.indexOf(numScale)];
+            if (scaleName && scaleName !== undefined) {
+                return handleThanHundredNums(numString, lenNum, scaleName, numScale);
+            }
+        }
+        return errors.unhandled;
     }
 }
 
 function handleThanHundredNums(numString, lenNum, scale, numRange) {
     const [minuend, startrange, midrange, _] = [numRange[0] - 1, ...numRange]
-    const [H, T] = [lenNum === startrange ? units[numString[0]] : lenNum === midrange ? convertPreDecimalNumber(numString.slice(0, 2)) : convertPreDecimalNumber(numString.slice(0, 3)), +numString.slice(lenNum - minuend)];
-    const tailString = T ? `${T >= 100 ? '' : 'and'} ${convertPreDecimalNumber(T.toString())}`.trim() : '';
+    const [H, T] = [
+        lenNum === startrange ? 
+            units[numString[0]] :
+            lenNum === midrange ?
+            convertPreDecimalNumber(numString.slice(0, 2)) :
+            convertPreDecimalNumber(numString.slice(0, 3)), 
+        BigInt(numString.slice(lenNum - minuend)).toString()
+    ];
+    const tailString = +T ? `${+T >= 100 ? '' : 'and'} ${convertPreDecimalNumber(T.toString())}`.trim() : '';
     return `${H} ${scale} ${tailString}`.trim()
 }
 
